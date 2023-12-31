d4272101a03db1baf2e68c4dda199eaa9e2034dc
synoap <devsynoap@gmail.com>
2023-12-30 23:14:22 -0300

    Feat: Animation Hamburger Menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
-import "/scss/style.scss";
+import '../scss/style.scss';
+
+const navToggle = document.querySelector('#navToggle');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+navToggle.addEventListener('click', () => {
+  console.log('Open the menu');
+
+  if (header.classList.contains('open')) { // Close the menu
+    header.classList.remove('open');
+    overlay.classList.remove('fade-in')
+    overlay.classList.add('fade-out')
+  } else { // Open the menu
+    header.classList.add('open');
+    overlay.classList.remove('fade-out')
+    overlay.classList.add('fade-in')
+  }
+})
